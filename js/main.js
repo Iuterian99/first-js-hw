9b799e539f3c7eb2147b2efeb4faf295f4798207
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2021-09-07 19:13:53 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert("So`rovnomaga Xush Kelibsiz!");
+var firstName = prompt('Ismingiz:', 'Abduhalim');
+var lastName = prompt('Familyangiz: ', 'Orziqulov');
+var telNumber = prompt('tel raqamiz: ', '+998999058838');
+var address = prompt('Yashash joyingiz: ', 'Yunusobod');
+var year = prompt('yoshingiz: ', '22');
+var email = prompt('emailingiz: ', 'Abduhalim.orziqulov@bk.ru');
+alert("So`rovnomani muvafaqqiyatli tamomladingiz! Rahmat :)");
+
+console.log("Ismim: " + firstName + "\nFamilyam: "+ lastName + "\nNomerim: " + telNumber + "\nYashash manzilim: " + address + "\nYoshim: " + year + "\nEmailim: " + email);
+
+console.log("Vaqtingiz va e`tiboringiz uchun rahmat!")
